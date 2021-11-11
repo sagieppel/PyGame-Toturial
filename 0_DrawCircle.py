534d1d7b21a96b1db2bdf1b8c1b84ff2ff0a1515
sagieppel <sagieppel@gmail.com>
2021-11-11 16:59:52 -0500

    Update 0_DrawCircle.py

--- 0_DrawCircle.py
+++ 0_DrawCircle.py
@@ -2,16 +2,12 @@ import pygame
 
 pygame.init()
 
-
 screen=pygame.display.set_mode([500,500])
 
 pygame.draw.circle(screen, [255,0,0], [100,100], 50)
 
-
 pygame.display.update()
 
 pygame.time.delay(10000)
-
-
 
 pygame.quit()
