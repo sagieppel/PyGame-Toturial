eb5bf061873e9c8f8c876f0de73ed0b4a1d3071b
sagieppel <sagieppel@gmail.com>
2021-11-11 17:52:36 -0500

    Update 0_DrawCircle.py

--- 0_DrawCircle.py
+++ 0_DrawCircle.py
@@ -2,12 +2,12 @@ import pygame
 import random
 
 #-------------------------------------------------------------------------
-def drawcircle():
+def drawcircle(x,y):
     red=random.randint(0,255)
     green=random.randint(0,255)
     blue=random.randint(0,255)
-    x=random.randint(0,500)
-    y=random.randint(0,500)
+   # x=random.randint(0,500)
+  #  y=random.randint(0,500)
     size=random.randint(1,30)
     pygame.draw.circle(screen, [red,green,blue], [x,y], size)
 #--------------------------------------------------------------------------
@@ -16,6 +16,10 @@ pygame.init()
 screensize=500
 
 screen=pygame.display.set_mode([screensize,screensize])
-for i in range(1000):
-   drawcircle()
-   pygame.display.update()
+
+
+for i in range(100000):
+   pygame.event.get()
+   x,y = pygame.mouse.get_pos()
+   drawcircle(x,y)
+   pygame.display.update(
