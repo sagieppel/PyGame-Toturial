26f3dbf684c79e5fb588698fb6ccb4dfe8dad396
sagieppel <sagieppel@gmail.com>
2021-11-11 16:42:08 -0500

    Add files via upload

---
+++ 0_DrawCircle.py
@@ -0,0 +1,17 @@
+import pygame 
+
+pygame.init()
+
+
+screen=pygame.display.set_mode([500,500])
+
+pygame.draw.circle(screen, [255,0,0], [100,100], 50)
+
+
+pygame.display.update()
+
+pygame.time.delay(10000)
+
+
+
+pygame.quit()
