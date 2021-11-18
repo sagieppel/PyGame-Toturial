4e810b7f9f6df9119f387efe1ec88fd26a83c5f7
sagieppel <sagieppel@gmail.com>
2021-11-18 17:24:02 -0500

    Update Lesson2.py

--- Lesson2.py
+++ Lesson2.py
@@ -1,25 +1,26 @@
 import pygame 
 import random
 
-#-------------------------------------------------------------------------
+#-----------------Draw a circle at X,Y--------------------------------------------------------
 def drawcircle(x,y):
-    red=random.randint(0,255)
-    green=random.randint(0,255)
-    blue=random.randint(0,255)
-   # x=random.randint(0,500)
-  #  y=random.randint(0,500)
-    size=random.randint(1,30)
-    pygame.draw.circle(screen, [red,green,blue], [x,y], size)
+    circle={}
+    circle["red"]=random.randint(0,255)
+    circle["green"]=random.randint(0,255)
+    circle["blue"]=random.randint(0,255)
+    circle["x"]=x #random.randint(0,255)
+    circle["y"]=y #random.randint(0,255) 
+    circle["size"]=random.randint(1,30)
+    pygame.draw.circle(screen, [circle["red"],circle["green"],circle["blue"]], [circle["x"],circle["y"]], circle["size"])
 #--------------------------------------------------------------------------
+
 pygame.init()
 
 screensize=500
 
 screen=pygame.display.set_mode([screensize,screensize])
 
-
-for i in range(100000):
+for i in range(1000000):
    pygame.event.get()
-   x,y = pygame.mouse.get_pos()
+   x,y = pygame.mouse.get_pos() # Get position of the mouse
    drawcircle(x,y)
    pygame.display.update()
