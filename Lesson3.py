beadf4587f5a4d72792a915a12991b4b2c4d79eb
sagieppel <sagieppel@gmail.com>
2021-11-25 17:31:25 -0500

    Update Lesson3.py

--- Lesson3.py
+++ Lesson3.py
@@ -9,13 +9,15 @@ def createcircle(x,y):
   circle["x"]=x
   circle["y"]=y 
   circle["size"]=random.randint(1,30)
-  circle["speedx"]=1
-  circle["speedy"]=-1
+  circle["speedx"]=3
+  circle["speedy"]=-5
+  circle["gravity"]=0.1
   return circle
 #------------------Draw a circle--------------------------------------------------------
 def updatecircle(circle):
     circle["x"] += circle ["speedx"]
     circle["y"] += circle ["speedy"]
+    circle["speedy"]+= circle["gravity"]
     pygame.draw.circle(screen, [circle["red"],circle["green"],circle["blue"]], [int(circle["x"]),int(circle["y"])], int(circle["size"]))
 
 #----------------initiate pygame---------------------------------------------------------    
