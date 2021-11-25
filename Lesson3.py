e653729a1b405fef6c6b39ec9724cb7cd5a8cf2a
sagieppel <sagieppel@gmail.com>
2021-11-25 17:27:10 -0500

    Update Lesson3.py

--- Lesson3.py
+++ Lesson3.py
@@ -9,23 +9,27 @@ def createcircle(x,y):
   circle["x"]=x
   circle["y"]=y 
   circle["size"]=random.randint(1,30)
+  circle["speedx"]=1
+  circle["speedy"]=-1
   return circle
 #------------------Draw a circle--------------------------------------------------------
-def drawcircle(circle):
+def updatecircle(circle):
+    circle["x"] += circle ["speedx"]
+    circle["y"] += circle ["speedy"]
     pygame.draw.circle(screen, [circle["red"],circle["green"],circle["blue"]], [int(circle["x"]),int(circle["y"])], int(circle["size"]))
-#----------------initiate pygame---------------------------------------------------------    
 
+#----------------initiate pygame---------------------------------------------------------    
 pygame.init()
-screensize=500
+screensize=700
 screen=pygame.display.set_mode([screensize,screensize]) # Set screen
 
-circle=createcircle(100,100)
+circle=createcircle(300,300)
 
 #----------------------------Main loop--------------------------------------------------------------------
 for i in range(10000000):
   #pygame.event.get() # Get user input
   #x,y = pygame.mouse.get_pos() # Get position of the mouse
   pygame.Surface.fill(screen,(0,0,0)) # Fill screen
-  drawcircle(circle)
+  updatecircle(circle)
   pygame.time.delay(10) # Wait 10 milli second
   pygame.display.update()
